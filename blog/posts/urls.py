8b2ee9c18c8d5592e637b94ad43989bbfe94f1d3
lubnaonline228 <46602183+lubnaonline228@users.noreply.github.com>
2024-11-16 00:30:03 +0600

    Add files via upload

--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+app_name = 'posts'
 
 urlpatterns = [
-    path("", views.posts_list),
+    path("", views.posts_list, name="list"),   #the name="posts" is used when you use a named link like {% url 'posts'%} in a template file to refer to the view
+    path("<slug:slug>", views.post_page, name="page"), #here, we have a path converter inside the <>. The left-side 'slug' is the path converter (it's a built-in keyword) and the right-side slug is the value for the slug (user-named) that we are passing to the post_page view
+                                                        #check the posts_lists.html file to see how this is referred
 ]
--- blog/posts/views.py
+++ blog/posts/views.py
@@ -7,3 +7,6 @@ def posts_list(request):
     posts=Post.objects.all().order_by('-date')  #when you add '-' sign before date, it orders the posts by date in descending order. So the latest blog is shown first
     return render(request, 'posts/posts_list.html', {'posts': posts})  # this passes a python dictionary with all my Post objects as data to the posts_list.html template
 
+def post_page(request, slug):
+    post=Post.objects.get(slug=slug)   #this will get only the post object where the slug field (that's on the left side of the '=') contains the value in slug (that's on the right side of the '='). The right side slug is obtained from the slug value passed to this function (post_page) in line 10
+    return render(request, 'posts/post_page.html', {'post': post})  
